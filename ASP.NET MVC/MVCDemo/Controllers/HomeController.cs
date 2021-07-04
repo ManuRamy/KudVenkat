c48d2ddedc3ddd41b8186c06fcfdcf14935c4ce4
ManuRamy <manjunath.sampangi@gmail.com>
2021-07-04 18:33:49 +0530

    added files

---
+++ ASP.NET MVC/MVCDemo/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ViewResult Index(string id, string name)
+		{
+			ViewBag.Countries =  new List<string>()
+			{
+				"India",
+				 "US",
+				 "England",
+				 "Germany"
+			};
+
+			return View();
+		}
+
+		public ActionResult About()
+		{
+			ViewBag.Message = "Your application description page.";
+
+			return View();
+		}
+
+		public ActionResult Contact()
+		{
+			ViewBag.Message = "Your contact page.";
+
+			return View();
+		}
+	}
+}
